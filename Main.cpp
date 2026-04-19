726e2aa74f2efe8608e6fb9ab722e7bdeab4efbc
Khan <amberkhalil222@gmail.com>
2026-04-19 11:17:46 -0700

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int main() {
     cout << "Library Management System\n";
+    cout << "Updated Version of Library System!" << endl;
     cout << "1. Add Book\n2. View Books\n";
     
     cout << "Welcome to Library System!" << endl;
