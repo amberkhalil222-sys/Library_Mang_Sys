029262af6684d4bb8c1e5b68ed4ab807200b80d9
Khan <amberkhalil222@gmail.com>
2026-04-19 11:04:00 -0700

    Initial commit - added main.cpp

---
+++ Main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Library Management System\n";
+    cout << "1. Add Book\n2. View Books\n";
+    
+    cout << "Welcome to Library System!" << endl;
+
+    return 0;
+}
